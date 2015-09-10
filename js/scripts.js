f0682567433c9342de5e545482a279815bc2639b
Alexa Zeazas & Michael Summers <student@epicodus.com>
2015-09-10 14:53:32 -0700

    Write code for returning list of words in order of frequency, pass.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,42 @@
-var downCase = function(input) {
-  var output = input.toLowerCase()
+var wordCount = function(text) {
+  var newArray = text.toLowerCase().replace(/[^\w\s]/g, "").split(" ");
+  var result = {};
+  var highest = 0;
+  var words = [];
+  var output = "";
+
+  newArray.forEach(function(word) {
+    if (result.hasOwnProperty(word)) {
+      //add 1 to it's value
+      result[word] += 1;
+      if (result[word] > highest) {
+        highest = result[word];
+      }
+    } else {
+      //create object and value of 1
+      result[word] = 1;
+      words.push(word);
+    }
+  });
+
+  for (var count = highest; count > 0; count--) {
+    words.forEach(function(word) {
+      if (result[word] === count) {
+        if (output == "") {
+          output = word;
+        } else {
+          output = output + ", " + word;
+        }
+      }
+    });
+  }
+
   return output;
 };
-
-var wordCount = function(input) {
-  input = downCase(input);
-  return input;
-};
+//
+// var wordCount = function(text) {
+//   //normalizes input
+//   var text = downCase(text);
+//
+//   return text;
+// };
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,13 @@
 describe("wordCount", function(input){
   it("downcases input for consistency", function() {
-    expect(wordCount("Ruby RUBY ruby")).to.equal("ruby ruby ruby");
+    expect(wordCount("Ruby RUBY ruby")).to.equal("ruby");
+  });
+
+  it("makes a word list (without duplicates) from input", function() {
+    expect(wordCount("javascript Ruby ruby")).to.equal("ruby, javascript")
+  });
+  
+  it("outputs list sorting by frequency", function() {
+    expect(wordCount("ruby ruby javascript html javascript ruby")).to.equal("ruby, javascript, html");
   });
 });
